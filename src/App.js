1aee5e5a4a0f109ce6929f9773d403b7ef8e32eb
AzmiHabib <azmihabib0323@gmail.com>
2022-04-07 01:15:49 +0600

    login logout btn working

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import './App.css';
-import app from './firebase.init';
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-
+import "./App.css";
+import app from "./firebase.init";
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+import { useState } from "react";
 
 const auth = getAuth(app);
 
 function App() {
-
   const provider = new GoogleAuthProvider();
+  const [user, setUser] = useState({});
 
-  const handleGoogleSignIn =()=>{
-    signInWithPopup(auth,provider)
-    .then(result=>{
-      console.log(result.user);
+  const handleGoogleSignIn = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const user = result.user;
+        setUser(user);
+        console.log(user);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  const handleGoogleSignOut = () => {
+    signOut(auth)
+    .then(()=>{
+      setUser({})
     })
     .catch(error=>{
-      console.log(error);
+      setUser({})
     })
-  }
+  };
   return (
     <div className="App">
-      <button onClick={handleGoogleSignIn}>Google Sign In</button>
+      {
+        !user.email ? <button onClick={handleGoogleSignIn}>Google Sign In</button> : <button onClick={handleGoogleSignOut}>Sign Out</button>
+      }
+      
+      
+      <h1>Name : {user.displayName}</h1>
     </div>
   );
 }
